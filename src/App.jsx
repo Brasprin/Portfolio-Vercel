06af9130d898f7fa2c8b5c4209edd2d4d817b22c
Andrei Tamse <andrei_tamse@dlsu.edu.ph>
2025-09-05 15:16:38 +0800

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import MainLayout from './components/MainLayout';
 
 import Home from './pages/Home';
@@ -13,6 +14,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
